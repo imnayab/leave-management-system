2a36eabfbc903ea5206908d23f15eac723d5d968
Shaik Nayab Rasool <somlamab@gmail.com>
2026-03-21 01:47:22 +0530

    fixed api url for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://16.176.2.233:5000/api" // 👈 IMPORTANT
+  baseURL: "http://16.176.2.233:5000/api", // 🔥 YOUR EC2 IP
 });
 
 export default api;
